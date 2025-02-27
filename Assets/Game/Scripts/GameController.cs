b2b3d945f6775999f9d7ccd19f5a35c21700b1b7
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-02-26 22:18:39 -0500

    Created quick setup to spawn 6 cards

--- Assets/Game/Scripts/GameController.cs
+++ Assets/Game/Scripts/GameController.cs
@@ -1,11 +1,22 @@
+using CozyDesigner.Card;
+using CozyDesigner.Game;
 using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] Transform _cardSpawnParent;
+    [SerializeField] GameObject _cardPrefab;
+
+    CardCreator _creator = new();
+
     void Start()
     {
-        
+        for (int i = 0; i < 6; i++)
+        {
+            var GO = Instantiate(_cardPrefab, _cardSpawnParent);
+            Card card = GO.GetComponentInChildren<Card>();
+            card.SetItem(_creator.BuildCard());
+        }
     }
 
     // Update is called once per frame
