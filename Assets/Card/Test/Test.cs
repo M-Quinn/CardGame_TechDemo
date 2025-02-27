b86cf35cacf55f884dc471fed7413c5442cfede0
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-02-26 19:30:31 -0500

    added a little test to pull the enums for a future switch

--- Assets/Card/Test/Test.cs
+++ Assets/Card/Test/Test.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace CozyDesigner.Card
 {
@@ -11,6 +13,11 @@ namespace CozyDesigner.Card
             chair = new Moldy(chair);
             chair = new Basic(chair);
             Debug.Log($"{chair.name}\nComfort: {chair.comfort}\nEsthetic: {chair.esthetic}\nDurability: {chair.durability}\nRarity: {chair.itemRarity}\nType: {chair.itemType}");
+
+            var values = Enum.GetValues(typeof(ItemAdjective));
+            ItemAdjective randAdjective = (ItemAdjective) values.GetValue(Random.Range(0, values.Length));
+
+            Debug.Log(randAdjective);
         }
 
         // Update is called once per frame
