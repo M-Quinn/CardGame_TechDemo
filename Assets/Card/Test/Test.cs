e2a056091dab45aaf571a8066cd29841ade90c8c
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-02-26 19:49:31 -0500

    Created a random item creation test

--- Assets/Card/Test/Test.cs
+++ Assets/Card/Test/Test.cs
@@ -14,16 +14,86 @@ namespace CozyDesigner.Card
             chair = new Basic(chair);
             Debug.Log($"{chair.name}\nComfort: {chair.comfort}\nEsthetic: {chair.esthetic}\nDurability: {chair.durability}\nRarity: {chair.itemRarity}\nType: {chair.itemType}");
 
-            var values = Enum.GetValues(typeof(ItemAdjective));
-            ItemAdjective randAdjective = (ItemAdjective) values.GetValue(Random.Range(0, values.Length));
+            Item randomCard = BuildCard();
 
-            Debug.Log(randAdjective);
+            Debug.Log($"{randomCard.name}\nComfort: {randomCard.comfort}\nEsthetic: {randomCard.esthetic}\nDurability: {randomCard.durability}\nRarity: {randomCard.itemRarity}\nType: {randomCard.itemType}");
         }
 
-        // Update is called once per frame
-        void Update()
+        Item BuildCard()
         {
+            var valuesAdj = Enum.GetValues(typeof(ItemAdjective));
+            ItemAdjective randAdjective = (ItemAdjective)valuesAdj.GetValue(Random.Range(0, valuesAdj.Length));
 
+            var valuesItem = Enum.GetValues(typeof(ItemType));
+            ItemType randItem = (ItemType)valuesItem.GetValue(Random.Range(0, valuesItem.Length));
+
+            var valuesRarity = Enum.GetValues(typeof(ItemRarity));
+            ItemRarity randRarity = (ItemRarity)valuesRarity.GetValue(Random.Range(0, valuesRarity.Length));
+
+
+            Item item;
+
+            switch (randItem)
+            {
+                case ItemType.CHAIR:
+                    item = new Chair();
+                    break;
+                case ItemType.COUCH:
+                    item = new Couch();
+                    break;
+                case ItemType.TABLE:
+                    item = new Table();
+                    break;
+                case ItemType.DESK:
+                    item = new Desk();
+                    break;
+                case ItemType.BED:
+                    item = new Bed();
+                    break;
+                default:
+                    item = new Chair();
+                    Debug.LogError($"Item type {randItem} failed");
+                    break;
+            }
+
+            switch (randAdjective)
+            {
+                case ItemAdjective.NONE:
+                    break;
+                case ItemAdjective.MOLDY:
+                    item = new Moldy(item);
+                    break;
+                case ItemAdjective.RIPPED:
+                    item = new Ripped(item);
+                    break;
+                case ItemAdjective.LEATHER:
+                    item = new Leather(item);
+                    break;
+                case ItemAdjective.REINFORCED:
+                    item = new Reinforced(item);
+                    break;
+                case ItemAdjective.WOODEN:
+                    item = new Wooden(item);
+                    break;
+                case ItemAdjective.METAL:
+                    item = new Metal(item);
+                    break;
+            }
+
+            switch (randRarity)
+            {
+                case ItemRarity.BASIC:
+                    item = new Basic(item);
+                    break;
+                case ItemRarity.COOL:
+                    item = new Cool(item);
+                    break;
+                case ItemRarity.LEGENDARY:
+                    item = new Legendary(item);
+                    break;
+            }
+
+            return item;
         }
     }
 }
