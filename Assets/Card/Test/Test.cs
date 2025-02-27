fbac1f727069c11beb893a27965495937f93580b
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-02-26 21:34:31 -0500

    confirmed card building test without ui

--- Assets/Card/Test/Test.cs
+++ Assets/Card/Test/Test.cs
@@ -12,13 +12,13 @@ namespace CozyDesigner.Card
             Item chair = new Chair();
             chair = new Moldy(chair);
             chair = new Basic(chair);
-            Debug.Log($"{chair.name}\nComfort: {chair.comfort}\nEsthetic: {chair.esthetic}\nDurability: {chair.durability}\nRarity: {chair.itemRarity}\nType: {chair.itemType}");
+            Debug.Log($"{chair.name}\nComfort: {chair.comfort}\nEsthetic: {chair.aesthetic}\nDurability: {chair.durability}\nRarity: {chair.itemRarity}\nType: {chair.itemType}");
 
             CardCreator cardCreator = new();
 
             Item randomCard = cardCreator.BuildCard();
 
-            Debug.Log($"{randomCard.name}\nComfort: {randomCard.comfort}\nEsthetic: {randomCard.esthetic}\nDurability: {randomCard.durability}\nRarity: {randomCard.itemRarity}\nType: {randomCard.itemType}");
+            Debug.Log($"{randomCard.name}\nComfort: {randomCard.comfort}\nEsthetic: {randomCard.aesthetic}\nDurability: {randomCard.durability}\nRarity: {randomCard.itemRarity}\nType: {randomCard.itemType}");
         }
 
         
