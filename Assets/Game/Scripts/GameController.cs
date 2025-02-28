55f6bdbbae135866b785df69cb5f06630413971e
Michael Quinn <47506506+M-Quinn@users.noreply.github.com>
2025-02-28 04:40:03 -0500

    Created a simple way to refresh the deck during runtime

--- Assets/Game/Scripts/GameController.cs
+++ Assets/Game/Scripts/GameController.cs
@@ -1,15 +1,39 @@
 using CozyDesigner.Card;
 using CozyDesigner.Game;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     [SerializeField] Transform _cardSpawnParent;
     [SerializeField] GameObject _cardPrefab;
+    [SerializeField] Button _button;
 
     CardCreator _creator = new();
 
     void Start()
+    {
+        RefreshDeck();
+    }
+
+    void OnEnable() => _button.onClick.AddListener(RefreshDeck);
+    void OnDisable() => _button.onClick.RemoveListener(RefreshDeck);
+
+    void RefreshDeck()
+    {
+        RemoveDeck();
+        BuildDeck();
+    }
+
+    void RemoveDeck()
+    {
+        while (_cardSpawnParent.childCount > 0)
+        {
+            DestroyImmediate(_cardSpawnParent.GetChild(0).gameObject);
+        }
+    }
+
+    void BuildDeck()
     {
         for (int i = 0; i < 6; i++)
         {
@@ -17,11 +41,5 @@ public class GameController : MonoBehaviour
             Card card = GO.GetComponentInChildren<Card>();
             card.SetItem(_creator.BuildCard());
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
